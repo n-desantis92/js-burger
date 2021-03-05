9f0795bec8ece5735f7d4c5ca0ddaeb6efed1c69
n-desantis92 <dsnwebdev@gmail.com>
2021-03-05 16:53:10 +0100

    aggiunto javascript

---
+++ script/script.js
@@ -0,0 +1,32 @@
+var calcolatore = document.getElementById('calculate');
+var burger = document.getElementById('input-burger-name');
+var ingredienti = document.getElementsByClassName('add-ingredient');
+var somma = 50;
+var coupons = ["a","b","c","vip"];
+var coupon = document.getElementById('coupon');
+var totale = document.getElementById('total');
+// evento al click del bottone
+calcolatore.addEventListener("click",
+  function () {
+    // controllo se viene inserito il nome del burger
+    if (burger != "") {
+      // controllo gli ingredienti
+      for (var i = 0; i < ingredienti.length; i++) {
+        // se la checkbox è spuntata allora verrà sommata
+        console.log(ingredienti[i].checked);
+        if (ingredienti[i].checked == true) {
+          // trasformo il value da stringa ad intero e lo sommo
+          somma += parseInt(ingredienti[i].value);
+        }
+      }
+      // ora applico lo sconto
+      if (coupons.includes(coupon.value)) {
+        somma -= somma * 0.2;
+      }
+      totale.innerHTML = somma.toFixed(2) + "€";
+    } else {
+      alert("inserisci il panino");
+    }
+  }
+
+);
